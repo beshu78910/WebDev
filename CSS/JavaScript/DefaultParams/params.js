06967081717584d30d1d57df63fecf5512310f0a
DuyAnh.Nguyen001@umb.edu <57365120+beshu78910@users.noreply.github.com>
2022-02-09 22:25:50 -0500

    Complete JS

--- CSS/JavaScript/DefaultParams/params.js
+++ CSS/JavaScript/DefaultParams/params.js
@@ -1,14 +1,28 @@
-const testArr = {
-    num1: 20,
-    num2: 30,
-    num3: 40,
-    num5: 50,
-    num6: 70,
+
+const users = {
+    name: 'Duy Anh Nguyen',
+    password: 10,
+    email: 'duyanh.nguyen001@umb.edu',
+    age: 23,
+    birthYear: 1999,
+    smoke: 'no',
+    hair: 'black'
 }
-// Order does not matter like arry, 
-// Store by name
-// const { num2, num2 } = testArr;
 
-// Renaming a key inside of object and store it in another key
-const { num2: birthYear, num3: deathYear} = testArr;
 
+// OLd way
+// function fullName(user) {
+//     return `${user.name} and ${user.hair}`
+// }
+// fullName(users);
+
+// Defracturing
+// function fullName(user) {
+//     const { name, age } = user;
+//     return `${name} and ${age}` //does not need user. anymore
+// }
+
+// Another way
+function fullName({ name, password }) {
+    return `${name}, ${password}`
+}
